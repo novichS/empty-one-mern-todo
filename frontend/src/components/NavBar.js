d1c344fcffbc414fbe7c7dc571ad4f84b40de06a
svasyanovych <svitlana.v92@gmail.com>
2020-06-05 12:01:59 +0300

    fix

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const NavBar = () => {
     const history = useHistory()
--- frontend/src/pages/EditPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const EditPage = () => {
-    return (
-        <div>
-            <h1>Edit Page</h1>
-        </div>
-    )
-}
